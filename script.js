bdb5b59e959d24ea6a192fb9e20a7ee258617275
Chm555556 <chandramohanmahto0170@gmail.com>
2024-01-15 23:19:14 +0530

    Chm

--- script.js
+++ script.js
@@ -15,23 +15,23 @@ $(document).ready(function(){
         }
     });
 
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
+    // // slide-up script
+    // $('.scroll-up-btn').click(function(){
+    //     $('html').animate({scrollTop: 0});
+    //     // removing smooth scroll on slide-up button click
+    //     $('html').css("scrollBehavior", "auto");
+    // });
 
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });
+    // $('.navbar .menu li a').click(function(){
+    //     // applying again smooth scroll on menu items click
+    //     $('html').css("scrollBehavior", "smooth");
+    // });
 
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
+    // // toggle menu/navbar script
+    // $('.menu-btn').click(function(){
+    //     $('.navbar .menu').toggleClass("active");
+    //     $('.menu-btn i').toggleClass("active");
+    // });
 
     // typing text animation script
     var typed = new Typed(".typing", {
